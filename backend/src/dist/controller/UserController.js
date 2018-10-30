b0d322049f0009700a85618df3584aa1bab348e0
Daniel Medeiros <danielmedeiros52@gmail.com>
2018-10-29 21:34:48 -0300

    ajustes de projeto

---
+++ backend/src/dist/controller/UserController.js
@@ -0,0 +1,65 @@
+
+class UserController {
+    cadastrar(res, req, app) {
+        let user = req.body
+        let data = [user.nome, user.senha, user.email, user.cpf]
+        createConection(app).localizarEmail([user.email], (err, result) => {
+
+            if (result.rowCount != 0) {
+                res.send('Usuário já existe !')
+            } else {
+                createConection(app).localizarCpf([user.cpf], (err, result) => {
+                    if (result.rowCount != 0) {
+                        res.send('Usuário já existe !')
+                    } else {
+                        inserir(res, data, createConection(app))
+                    }
+                })
+            }
+        }
+
+
+        )
+    }
+    login(res, req, app) {
+        let user = req.body
+        let data = [user.email]
+        createConection(app).localizarEmail(data, (err, result) => {
+            if (err) {
+                console.log(err)
+            }
+            if (result.rowCount != 0) {
+                if (user.senha == result.rows[0].senha) {
+                    req.session.user = result.rows[0]
+                    res.send('/dashboard')
+                } else {
+                    res.send('login ou senha invalidos')
+                }
+
+
+
+            } else {
+                res.send('login ou senha invalidos')
+            }
+
+        })
+
+    }
+
+}
+function inserir(res, data, dao) {
+    dao.inserir(data, (err, resultado) => {
+        if (err) {
+            res.send(err)
+        }
+        res.send(resultado)
+    })
+}
+function createConection(app) {
+    const pool = app.infra.connectionFactory()
+    let UsuarioDAO = new app.infra.UsuarioDAO(pool)
+    return UsuarioDAO
+}
+module.exports = function (app) {
+    return UserController;
+}
---
+++ backend/src/dist/database/UserDAO.js
@@ -0,0 +1,25 @@
+class userDAO {
+    constructor(connection) {
+        this._connection = connection;
+    }
+    inserir(data, callback) {
+        this._connection.query('INSERT INTO USUARIO (nome,senha,email,cpf) VALUES ($1,$2,$3,$4)', data, callback);
+    }
+    localizarNome(data, callback) {
+        this._connection.query('SELECT * FROM usuario WHERE  nome ILIKE  $1', data, callback);
+    }
+    localizarEmail(data, callback) {
+        this._connection.query('SELECT * FROM usuario WHERE  email ILIKE  $1', data, callback);
+    }
+    localizarCpf(data, callback) {
+        this._connection.query('SELECT * FROM usuario WHERE  cpf =  $1', data, callback);
+    }
+    localizarMatricula(data, callback) {
+        this._connection.query('SELECT * FROM usuario WHERE  matricula ILIKE  $1', data, callback);
+    }
+}
+
+
+module.exports = function () {
+    return userDAO;
+}
